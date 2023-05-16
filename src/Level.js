e295e69e7c09b318a46524228a4c50fe436f704c
Your Name <niktjawalla922@gmail.com>
2023-05-17 01:11:59 +0200

    add axe

--- src/Level.js
+++ src/Level.js
@@ -60,6 +60,25 @@ function BlockLimbo({position=[0,0,0]})
     </group>
 }
 
+function BlockAxe({position=[0,0,0]})
+{
+    const obstacle = useRef()
+    const [timeOffset] = useState(()=>(Math.random() * Math.PI *2)) 
+
+    useFrame((state)=>{
+        const time = state.clock.getElapsedTime()
+        const x = Math.sin(time+ timeOffset) * 1.25
+        obstacle.current.setNextKinematicTranslation({x:x, y: position[1]+0.75, z: position[2]})
+    }
+    )
+
+    return <group position={position}>
+        <mesh geometry={boxGeometry} material={floor2Material} position={[0, -0.1, 0]} scale={[4,0.2,4]} receiveShadow></mesh>
+        <RigidBody ref={obstacle} type="kinematicPosition" position={[0,0.3,0]} restitution={0.2} friction={0} >
+        <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[1.5,1.5,0.3]} castShadow receiveShadow></mesh>
+        </RigidBody>
+    </group>
+}
 
 export default function level()
 {
@@ -67,6 +86,7 @@ export default function level()
     <BlockStart position={[0,0,12]}/>
     <BlockSpinner position={[0,0,8]}/>  
     <BlockLimbo position={[0,0,4]}/>
+    <BlockAxe position={[0,0,0]}/>
     </>
     
 }
