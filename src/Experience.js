bb00ceab144015d5c720d9a38e63ba7e7a51a861
Your Name <niktjawalla922@gmail.com>
2023-05-17 00:11:53 +0200

    add leveljs

--- src/Experience.js
+++ src/Experience.js
@@ -1,28 +1,15 @@
 import { OrbitControls } from '@react-three/drei'
 import Lights from './Lights.js'
+import Level from './Level.js'
 
 export default function Experience()
 {
     return <>
 
         <OrbitControls makeDefault />
-
+        <Level/>
         <Lights />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+       
     </>
 }
---
+++ src/Level.js
@@ -0,0 +1,23 @@
+export default function level()
+{
+    return(    
+    <>
+    <mesh castShadow position-x={ - 2 }>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+    </mesh>
+    
+    <mesh castShadow position-x={ 2 } scale={ 1.5 }>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+    </mesh>
+    
+    <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+    </mesh>
+    </>
+    
+    )
+
+}
