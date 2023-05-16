22bb1ed15d976a2639a7ef86a4e6c606508813df
Your Name <niktjawalla922@gmail.com>
2023-05-17 00:59:35 +0200

    add spinner

--- src/Level.js
+++ src/Level.js
@@ -1,19 +1,42 @@
 import * as THREE from 'three'
+import { RigidBody } from '@react-three/rapier'
+import { useState, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 
 THREE.ColorManagement.legacyMode = false
 
 const boxGeometry = new THREE.BoxGeometry(1,1,1)
 const floor1Material = new THREE.MeshStandardMaterial({color:'limegreen'})
-const floor2Material = new THREE.MeshStandardMaterial({color:'limegreen'})
+const floor2Material = new THREE.MeshStandardMaterial({color:'greenyellow'})
 const obstacleMaterial = new THREE.MeshStandardMaterial({color:'orangered'})
 const wallMaterial = new THREE.MeshStandardMaterial({color:'slategrey'})
 
 function BlockStart({position=[0,0,0]})
 {
     return <group position={position}>
-        <mesh geometry={boxGeometry} material={floor1Material} position={[0, -0.1, 0]} scale={[4,0.2,4]} receiveShadow>
-            <meshStandardMaterial color="limegreen"/>
-        </mesh>
+        <mesh geometry={boxGeometry} material={floor1Material} position={[0, -0.1, 0]} scale={[4,0.2,4]} receiveShadow></mesh>
+    </group>
+}
+
+function BlockSpinner({position=[0,0,0]})
+{
+    const obstacle = useRef()
+    const [speed] = useState(()=>(Math.random() + 0.2)*Math.random()<0.5 ? -1 : 1) 
+
+    useFrame((state)=>{
+        const time = state.clock.getElapsedTime()
+
+        const rotation = new THREE.Quaternion()
+        rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
+        obstacle.current.setNextKinematicRotation(rotation)
+    }
+    )
+
+    return <group position={position}>
+        <mesh geometry={boxGeometry} material={floor2Material} position={[0, -0.1, 0]} scale={[4,0.2,4]} receiveShadow></mesh>
+        <RigidBody ref={obstacle} type="kinematicPosition" position={[0,0.3,0]} restitution={0.2} friction={0} >
+        <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5,0.3,0.3]} castShadow receiveShadow></mesh>
+        </RigidBody>
     </group>
 }
 
@@ -21,7 +44,8 @@ function BlockStart({position=[0,0,0]})
 export default function level()
 {
     return <>
-    <BlockStart position={[0,0,0]}/>
+    <BlockStart position={[0,0,4]}/>
+    <BlockSpinner position={[0,0,0]}/>  
     </>
     
 }
