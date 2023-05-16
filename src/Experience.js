847de29d79b22976220f08136214a12b7ffc4b5b
Your Name <niktjawalla922@gmail.com>
2023-05-17 00:14:59 +0200

    add physics

--- src/Experience.js
+++ src/Experience.js
@@ -1,4 +1,5 @@
 import { OrbitControls } from '@react-three/drei'
+import { Physics, Debug } from '@react-three/rapier'
 import Lights from './Lights.js'
 import Level from './Level.js'
 
@@ -7,8 +8,12 @@ export default function Experience()
     return <>
 
         <OrbitControls makeDefault />
+
+        <Physics>
+        <Debug/>
         <Level/>
         <Lights />
+        </Physics>
 
        
     </>
