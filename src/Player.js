c9b9370658b5e3c75c5c40df3fa59c8c6791c22a
Your Name <niktjawalla922@gmail.com>
2023-06-20 01:38:06 +0200

    add controls

--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,85 @@
-import { RigidBody } from "@react-three/rapier"
+import { RigidBody, useRapier } from "@react-three/rapier"
+import { useFrame } from "@react-three/fiber"
+import { useKeyboardControls } from "@react-three/drei"
+import { useEffect, useRef } from "react"
 
 export default function Player()
-{
+{   
+    const body = useRef()
+    const [subcribeKeys, getKeys] = useKeyboardControls()
+    const {rapier, world} = useRapier()
+    const rapierWorld = world.raw()
+
+    const jump = () =>
+    {
+        const origin = body.current.translation()
+        origin.y -= 0.31
+        const direction = {x: 0, y:-1, z:0}
+        const ray = new rapier.Ray(origin, direction)
+        const hit = rapierWorld.castRay(ray, 10, true)
+
+        if (hit.toi < 0.15){
+            body.current.applyImpulse({ x:0, y: 0.5, z:0})
+        }
+    }
+
+    useEffect(()=>{
+        const unsubscribeJump = subcribeKeys(
+        (state)=> state.jump,
+        (value)=>
+        {
+            if(value)
+                jump()
+        })
+        return ()=>
+        {
+            unsubscribeJump()
+        }
+    },[])
+
+    useFrame((state, delta)=>
+    {
+        const {forward, backward, leftward, rightward} = getKeys()
+
+        const impulse = {x:0, y:0, z:0}
+        const torque = {x:0, y:0, z:0}
+
+        const impulseStrength = 0.6 * delta
+        const torqueStrength = 0.2 * delta
+
+        if(forward){
+            impulse.z -=impulseStrength
+            torque.x -= torqueStrength
+        }
+
+        if(rightward){
+            impulse.x +=impulseStrength
+            torque.z -= torqueStrength
+        }
+
+        if(backward){
+            impulse.z +=impulseStrength
+            torque.x += torqueStrength
+        }
+
+        if(leftward){
+            impulse.x -=impulseStrength
+            torque.z += torqueStrength
+        }
+
+        body.current.applyImpulse(impulse)
+        body.current.applyTorqueImpulse(torque)
+    })
+
     return <>
-    <RigidBody colliders="ball" restitution={0.2} friction={1} position={[0,1,0]}>
+    <RigidBody 
+        ref={body} 
+        colliders="ball" 
+        restitution={0.2} friction={1} 
+        position={[0,1,0]}
+        linearDamping={0.5}
+        angularDamping={0.5}
+    >
         <mesh castShadow>
             <icosahedronGeometry args={[0.3,1]}/>
             <meshStandardMaterial color= "mediumpurple"/>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.js'
+import { KeyboardControls } from '@react-three/drei'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
+<KeyboardControls 
+    map={[
+        {name: 'forward', keys:['ArrowUp', 'KeyW']},
+        {name: 'backward', keys:['ArrowDown', 'KeyS']},
+        {name: 'leftward', keys:['ArrowLeft', 'KeyA']},
+        {name: 'rightward', keys:['ArrowRight', 'KeyD']},
+        {name: 'jump', keys:['Space']}
+    ]}
+>
     <Canvas
         shadows
         camera={ {
@@ -17,4 +27,5 @@ root.render(
     >
         <Experience />
     </Canvas>
+</KeyboardControls>
 )
