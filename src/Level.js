902ada66166c4d4413cb1c08759d960a269a6310
Your Name <niktjawalla922@gmail.com>
2023-06-20 00:22:17 +0200

    add bounds

--- src/Level.js
+++ src/Level.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { RigidBody } from '@react-three/rapier'
+import { CuboidCollider, RigidBody } from '@react-three/rapier'
 import { useMemo, useState, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
@@ -97,6 +97,40 @@ export function BlockAxe({position=[0,0,0]})
     </group>
 }
 
+export function Bounds({length=1}){
+return<>
+    <RigidBody type='fixed' restitution={0.2} friction={0}>
+    <mesh 
+        position={[2.15, 0.75, -(length*2)+2]}
+        geometry={ boxGeometry}
+        material={ wallMaterial}
+        scale={[0.3, 1.5, 4*length]}
+        castShadow
+    />
+    <mesh 
+        position={[-2.15, 0.75, -(length*2)+2]}
+        geometry={ boxGeometry}
+        material={ wallMaterial}
+        scale={[0.3, 1.5, 4*length]}
+        receiveShadow
+    />
+    <mesh 
+        position={[0, 0.75, -(length*4)+2 ]}
+        geometry={ boxGeometry}
+        material={ wallMaterial}
+        scale={[4, 1.5, 0.3]}
+        receiveShadow
+    />
+    <CuboidCollider 
+        args={[2, 0.1 , 2*length]}
+        position={[0, -0.1, -(length*2)+2]}
+        restitution={0.2}
+        friction={1}
+    />
+    </RigidBody>
+</>
+}
+
 export function Level({count = 5, types=[BlockSpinner, BlockAxe, BlockLimbo]})
 {
     const blocks = useMemo(()=>
@@ -115,6 +149,8 @@ export function Level({count = 5, types=[BlockSpinner, BlockAxe, BlockLimbo]})
     <BlockStart position={[0,0,0]}/>
     {blocks.map((Block, index)=><Block key={index} position={[0,0,-(index+1)*4]}/>)}
     <BlockEnd position={[0,0,-(count+1)*4]}/>
+
+    <Bounds length={ count + 2}/>
     </>
     
 }
