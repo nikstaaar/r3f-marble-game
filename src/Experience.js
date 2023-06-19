76953fa5591541470e5959e4b5f94adbe368c2f6
Your Name <niktjawalla922@gmail.com>
2023-06-20 00:32:04 +0200

    add player

--- src/Experience.js
+++ src/Experience.js
@@ -2,6 +2,7 @@ import { OrbitControls } from '@react-three/drei'
 import { Physics, Debug } from '@react-three/rapier'
 import Lights from './Lights.js'
 import { Level } from './Level.js'
+import Player from './Player.js'
 
 export default function Experience()
 {
@@ -12,7 +13,8 @@ export default function Experience()
         <Physics>
         <Debug/>
         <Level/>
-        <Lights />
+        <Lights/>
+        <Player/>
         </Physics>
 
        
---
+++ src/Player.js
@@ -0,0 +1,13 @@
+import { RigidBody } from "@react-three/rapier"
+
+export default function Player()
+{
+    return <>
+    <RigidBody colliders="ball" restitution={0.2} friction={1} position={[0,1,0]}>
+        <mesh castShadow>
+            <icosahedronGeometry args={[0.3,1]}/>
+            <meshStandardMaterial color= "mediumpurple"/>
+        </mesh>
+    </RigidBody>
+    </>
+}
